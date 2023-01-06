f05abab0b1aca77407ca837bc0aa6c43c8147eb3
TR Kizaki <trkizaki.web@gmail.com>
2023-01-06 07:00:50 +0400

    added cartTotal method

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,17 +1,23 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { CartContext } from '../../contexts/cart.context';
 
 import React from 'react';
 
 import Button from '../button/button.component';
-
 import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    navigate('/checkout');
+  }
+
  return (
   <div className='cart-dropdown-container'>
     <div className='cart-items'>
@@ -20,7 +26,7 @@ const CartDropdown = () => {
       ))}
       </div>
 
-    <Button>GO TO CHECKOUT</Button>
+    <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
   </div>
    );
  };
